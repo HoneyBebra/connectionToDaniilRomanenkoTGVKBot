d446ef8cb5bff663dd84a833c5c3ef07e846a0eb
HoneyBebra <128954566+HoneyBebra@users.noreply.github.com>
2024-01-02 12:44:58 +1000

    Exception processing

--- bots/messages_sender.py
+++ bots/messages_sender.py
@@ -1,6 +1,5 @@
 import requests
 import random
-import vk_api
 import os
 import json
 
@@ -21,24 +20,26 @@ class VKMessage(Message):
         photo = vk_upload.photo_messages(photos=sticker_file)[0]
         self.attachment = f"photo{photo['owner_id']}_{photo['id']}"
 
-    async def _upload_animated_sticker_on_server(self, file_path) -> None:
+    async def _upload_video_sticker_on_server(self, file_path) -> None:
         # TODO: convert to .gif or .mp4
         file_url = f'https://api.telegram.org/file/bot{os.getenv("TG_TOKEN")}/{file_path}'
-        file = {'file': ('animated_sticker.webm', requests.get(file_url).content)}
+        file = {'file': ('video_sticker.webm', requests.get(file_url).content)}
         upload_url = vk.docs.getMessagesUploadServer(peer_id=os.getenv('PASHA_SERYI_VK_ID'))['upload_url']
         response = requests.post(upload_url, files=file)
         result = json.loads(response.text)
-        saved_file = vk.docs.save(file=result['file'], title='animated_sticker.webm')
+        saved_file = vk.docs.save(file=result['file'], title='video_sticker.webm')
         self.attachment = 'doc{}_{}'.format(saved_file['doc']['owner_id'], saved_file['doc']['id'])
 
     async def send(self) -> None:
         if self.sticker is not None:
             file = await tg_bot.get_file(self.sticker.file_id)
             file_path = file.file_path
-            try:
+            if self.sticker.is_video:
+                await self._upload_video_sticker_on_server(file_path)
+            elif self.sticker.is_animated:
+                self.message = 'Поддержка анимированных стикеров в разработке'
+            else:
                 await self._upload_static_sticker_on_server(file_path)
-            except vk_api.ApiError:
-                await self._upload_animated_sticker_on_server(file_path)
 
         vk.messages.send(
             user_id=os.getenv('PASHA_SERYI_VK_ID'),
