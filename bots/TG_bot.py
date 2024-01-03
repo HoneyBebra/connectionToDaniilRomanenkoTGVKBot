6035c593f1540cc3e750d766d37cca0eb68081c6
HoneyBebra <128954566+HoneyBebra@users.noreply.github.com>
2024-01-03 22:05:10 +1000

    Added voices, photos, circles processing

--- bots/TG_bot.py
+++ bots/TG_bot.py
@@ -8,8 +8,11 @@ from bots import dp, tg_bot
 @dp.message()
 async def echo_handler(message: types.Message) -> None:
     vk_message = VKMessage(
-        message=message.text, sticker=message.sticker,
-        voice=message.voice
+        message=message.caption if message.caption is not None else message.text,
+        sticker=message.sticker,
+        voice=message.voice,
+        photo=message.photo,
+        video_circle=message.video_note
     )
     await vk_message.send()
 
--- bots/messages_sender.py
+++ bots/messages_sender.py
@@ -9,17 +9,19 @@ from bots import tg_bot, vk, vk_upload
 class Message:
     def __init__(
             self, message: str, sticker=None,
-            voice=None
+            voice=None, photo=None, video_circle=None
     ):
         self.message = message
         self.sticker = sticker
         self.voice = voice
+        self.photo = photo
+        self.video_circle = video_circle
 
         self.attachment = None
 
 
 class VKMessage(Message):
-    async def _upload_static_sticker_on_server(self, sticker_file_path) -> None:
+    async def _upload_photo_on_server(self, sticker_file_path) -> None:
         sticker_file = await tg_bot.download_file(sticker_file_path)
         photo = vk_upload.photo_messages(photos=sticker_file)[0]
         self.attachment = f"photo{photo['owner_id']}_{photo['id']}"
@@ -28,11 +30,13 @@ class VKMessage(Message):
         # TODO: convert to .gif or .mp4
         file_url = f'https://api.telegram.org/file/bot{os.getenv("TG_TOKEN")}/{sticker_file_path}'
         file = {'file': ('video_sticker.webm', requests.get(file_url).content)}
-        upload_url = vk.docs.getMessagesUploadServer(peer_id=os.getenv('PASHA_SERYI_VK_ID'))['upload_url']
+        upload_url = vk.docs.getMessagesUploadServer(
+            peer_id=os.getenv('PASHA_SERYI_VK_ID')
+        )['upload_url']
         response = requests.post(upload_url, files=file)
         result = json.loads(response.text)
-        saved_file = vk.docs.save(file=result['file'], title='video_sticker.webm')
-        self.attachment = 'doc{}_{}'.format(saved_file['doc']['owner_id'], saved_file['doc']['id'])
+        saved_file = vk.docs.save(file=result['file'], title='video_sticker.webm')['doc']
+        self.attachment = f"doc{saved_file['owner_id']}_{saved_file['id']}"
 
     async def _sticker_check(self) -> None:
         if self.sticker is not None:
@@ -43,15 +47,41 @@ class VKMessage(Message):
             elif self.sticker.is_animated:
                 self.message = 'Поддержка анимированных стикеров в разработке'
             else:
-                await self._upload_static_sticker_on_server(sticker_file_path)
+                await self._upload_photo_on_server(sticker_file_path)
 
     async def _voice_message_check(self):
         if self.voice:
-            pass
+            voice_file = await tg_bot.get_file(self.voice.file_id)
+            voice_file_path = voice_file.file_path
+            voice = await tg_bot.download_file(voice_file_path)
+
+            # TODO: can't send a bytes object, correct
+            with open('temp_voice/voice.mp3', 'wb') as f:
+                f.write(voice.getbuffer())
+
+            voice = vk_upload.audio_message(
+                'temp_voice/voice.mp3', peer_id=os.getenv('PASHA_SERYI_VK_ID')
+            )['audio_message']
+            self.attachment = f"audio_message{voice['owner_id']}_{voice['id']}"
+
+    async def _photo_message_check(self):
+        if self.photo:
+            photo = await tg_bot.get_file(self.photo[-1].file_id)
+            photo_path = photo.file_path
+            await self._upload_photo_on_server(photo_path)
+
+    async def _video_circle_check(self):
+        if self.video_circle:
+            circle_file = await tg_bot.get_file(self.video_circle.file_id)
+            circle_file_path = circle_file.file_path
+            circle = await tg_bot.download_file(circle_file_path)
+            print(circle)
 
     async def send(self) -> None:
         await self._sticker_check()
         await self._voice_message_check()
+        await self._photo_message_check()
+        await self._video_circle_check()
 
         vk.messages.send(
             user_id=os.getenv('PASHA_SERYI_VK_ID'),
